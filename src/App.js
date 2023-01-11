a1743e51e2f669424810edde0efa4371b6eab237
Justus <justus.onyancha@moringaschool.com>
2023-01-11 11:59:50 +0300

    add props in app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import './App.css';
 
 // this fuction becomes a component 
 export default function App(){
-  return (
-    
-    <>
-      <h1>My Fiancee Princess Diana</h1>
-      <p>God answer my dreams</p>
-    </>
 
-  );
+const props = {
+  id: 'input',
+  type: 'text',
+  maxLenght: '3'
 }
+return (
+  <div>
+    <label>Input: </label>
+    <input {...props}  placeholder = 'User' />
+
+  </div>
+);
+}
+// return error ? <h1>error</h1> : <h2>success</h2>
+
+
+// const name = "Kemunto";
+
+//   return (
+    
+//     <>
+//       <h1>My Fiancee Princess Diana {name.toLowerCase()} </h1>
+//       <p>God answer my dreams</p>
+//     </>
+
+//   );
+
 
